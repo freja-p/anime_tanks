9c50ce23abfe2199b937171a8c389c91f96bdea7
Deculsion <danasanma@gmail.com>
2025-03-13 20:33:07 +0800

    Port to 4.4

--- scripts/DebugAutoload.gd
+++ scripts/DebugAutoload.gd
@@ -3,3 +3,8 @@ extends Node
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
 		get_tree().quit()
+	if event.is_action_pressed("debug_f3"):
+		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		else:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
