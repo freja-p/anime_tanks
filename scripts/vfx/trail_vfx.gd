78245cd455e03d79f3ab6b51b6a0871e3614c445
Deculsion <danasanma@gmail.com>
2025-05-08 11:27:05 +0800

    WIP Trail fx

--- scripts/vfx/trail_vfx.gd
+++ scripts/vfx/trail_vfx.gd
@@ -3,7 +3,7 @@ extends VFX
 
 @export var trail_duration : float = 0.8
 var speed : float
-var skip_frames : int = 2
+var skip_frames : int = 3
 
 
 func _process(delta: float) -> void:
@@ -16,8 +16,12 @@ func _process(delta: float) -> void:
 func play(start_point : Vector3, initial_basis : Basis,  end_point : Vector3 = start_point) -> void:
 	super(start_point, initial_basis, end_point)
 	speed = start_point.distance_to(end_point) / vfx_data.duration
+	get_tree().create_timer(vfx_data.duration).timeout.connect(_on_duration_end)
 	get_tree().create_timer(vfx_data.duration + trail_duration).timeout.connect(_on_timer_timeout)
 	
+func _on_duration_end() -> void:
+	global_position = end_point
+	skip_frames = 10000
 
 func _on_timer_timeout() -> void:
 	queue_free()
